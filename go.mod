5adc875f65799fab3d8eab343c111eb8baaf0acb
Robin Broda <robin@broda.me>
2020-04-29 20:09:39 +0200

    Add host key verification

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	fyne.io/fyne v1.2.4
-	github.com/coderobe/securenet v0.0.0-20200428194313-7f08e728ac22
+	github.com/coderobe/securenet v0.0.0-20200429180608-6443cef849ee
+	github.com/sethvargo/go-diceware v0.2.0
 	github.com/vmihailenco/msgpack/v4 v4.3.11
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math/big"
 	"net"
 	"net/url"
-	"errors"
+	"strings"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
@@ -12,11 +14,12 @@ import (
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 	"github.com/coderobe/securenet"
+	"github.com/sethvargo/go-diceware/diceware"
 	"github.com/vmihailenco/msgpack/v4"
 )
 
 type User struct {
-	Name string
+	Name     string
 	Password string
 }
 
@@ -39,59 +42,147 @@ func boundSendMessage(encoder *msgpack.Encoder, conn net.Conn) func(packetID int
 }
 
 func networkHost(server string) (err error) {
+	defer setContainer(hostContainer, hostScreen())
 	listener, err := net.Listen("tcp", server)
 	if err != nil {
-		panic("can't listen")
+		fmt.Println("Can't listen")
+		return
 	}
 
+	pub, priv, elligator, err := securenet.GenerateKeys()
+	if err != nil {
+		fmt.Println("Failed to generate host keys")
+		return
+	}
+
+	setContainer(hostContainer, messageScreen("Host", "Accepting connections"))
 	for {
 		pConn, err := listener.Accept()
-		conn, err := securenet.Wrap(pConn)
 		if err != nil {
-			return err
+			continue
 		}
-		var sentPing MessagePing // hold on to last ping we sent for pong
-		sendMessage := boundSendMessage(msgpack.NewEncoder(conn), conn)
-		decoder := msgpack.NewDecoder(conn)
-
-		sentPing.Token = "Foo, bar!"
-		sendMessage(packetPing, sentPing)
-
-		for {
-			messageByte, err := conn.ReadByte()
+		fmt.Println("Got new connection")
+		go func() {
+			conn, err := securenet.WrapWithKeys(pConn, pub, priv, elligator)
 			if err != nil {
-				var netErr net.Error
-				if errors.As(err, netErr) {
-					if netErr.Timeout() {
-						fmt.Println("Read timed out")
-						continue
-					}
-				}
-				panic("BBBBBBBBBBBBBBBBBBBBBb")
+				return
 			}
-			switch messageType := uint8(messageByte); messageType {
-			case packetPing:
-				var ping MessagePing
-				decoder.Decode(&ping)
+			var sentPing MessagePing // hold on to last ping we sent for pong
+			sendMessage := boundSendMessage(msgpack.NewEncoder(conn), conn)
+			decoder := msgpack.NewDecoder(conn)
 
-				fmt.Printf("Got ping, token is '%s'\n", ping.Token)
-				sendMessage(packetPong, ping) // return as pong
-			case packetPong:
-				var pong MessagePong
-				decoder.Decode(&pong)
-				fmt.Printf("Got pong %s, matches? %b\n", pong.Token, sentPing.Token == pong.Token)
-			default:
-				fmt.Printf("Unknown packet of type %d incoming\n", messageType)
+			sentPing.Token = "Foo, bar!"
+			sendMessage(packetPing, sentPing)
+
+			for {
+				messageByte, err := conn.ReadByte()
+				if err != nil {
+					var netErr net.Error
+					if errors.As(err, netErr) {
+						if netErr.Timeout() {
+							fmt.Println("Read timed out")
+							continue
+						}
+					}
+					panic("BBBBBBBBBBBBBBBBBBBBBb")
+				}
+				switch messageType := uint8(messageByte); messageType {
+				case packetPing:
+					var ping MessagePing
+					decoder.Decode(&ping)
+
+					fmt.Printf("Got ping, token is '%s'\n", ping.Token)
+					sendMessage(packetPong, ping) // return as pong
+				case packetPong:
+					var pong MessagePong
+					decoder.Decode(&pong)
+					fmt.Printf("Got pong, token is '%s', matches? %b\n", pong.Token, sentPing.Token == pong.Token)
+				default:
+					fmt.Printf("Unknown packet of type %d incoming\n", messageType)
+				}
 			}
-		}
+		}()
 	}
 }
 
+func bytesToDiceware(in []byte) (output string) {
+	wordlist := diceware.WordListEffLarge()
+	digits := wordlist.Digits()
+	input := big.NewInt(0)
+	input.SetBytes(in)
+
+	outword := 0
+	outlen := digits
+	for _, c := range input.Text(6) {
+		if outlen > 0 {
+			outword = outword*10 + (int(c) - 47)
+			outlen--
+		} else {
+			outlen = digits
+			output += " "
+			output += wordlist.WordAt(outword)
+			outword = 0
+		}
+	}
+	if outlen > 0 {
+		for outlen > 0 {
+			outword = outword*10 + 1
+			outlen--
+		}
+		output += " "
+		output += wordlist.WordAt(outword)
+	}
+	return output[1:]
+}
+
 func networkJoin(server string, user User) (err error) {
+	defer setContainer(joinContainer, joinScreen())
+	fmt.Println("Joining", server)
 	conn, err := securenet.Dial("tcp", server)
 	if err != nil {
 		return
 	}
+
+	abortChan := make(chan bool)
+	serverKeyWords := strings.Split(bytesToDiceware(conn.GetServerPublicKey()[:]), " ")
+	serverKey := ""
+	for i, k := range serverKeyWords {
+		serverKey += k
+		if (i+1)%4 == 0 {
+			serverKey += "\n"
+		} else {
+			serverKey += " "
+		}
+	}
+
+	setContainer(joinContainer, widget.NewGroup("Confirm network keys",
+		widget.NewVBox(
+			widget.NewLabelWithStyle("The host is presenting this key:", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+			layout.NewSpacer(),
+			widget.NewLabelWithStyle(serverKey, fyne.TextAlignCenter, fyne.TextStyle{Monospace: true}),
+			layout.NewSpacer(),
+			widget.NewLabelWithStyle("If this is not the same key the host sees,\nyour connection might be intercepted.", fyne.TextAlignCenter, fyne.TextStyle{Italic: true}),
+			layout.NewSpacer(),
+
+			widget.NewGroup("Continue connecting?",
+				fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+					widget.NewButton("No", func() {
+						fmt.Println("Cancelled connection")
+						abortChan <- true
+					}),
+					widget.NewButton("Yes", func() {
+						fmt.Println("Continuing connection")
+						abortChan <- false
+					}),
+				),
+			),
+		),
+	))
+	if <-abortChan {
+		return
+	}
+	setContainer(joinContainer, messageScreen("Join", "Connected"))
+
 	var sentPing MessagePing // hold on to last ping we sent for pong
 	sendMessage := boundSendMessage(msgpack.NewEncoder(conn), conn)
 	decoder := msgpack.NewDecoder(conn)
@@ -100,7 +191,7 @@ func networkJoin(server string, user User) (err error) {
 		messageByte, err := conn.ReadByte()
 		if err != nil {
 			var netErr net.Error
-			if errors.As(err, netErr) {
+			if errors.As(err, &netErr) {
 				if netErr.Timeout() {
 					fmt.Println("Read timed out")
 					continue
@@ -118,7 +209,7 @@ func networkJoin(server string, user User) (err error) {
 		case packetPong:
 			var pong MessagePong
 			decoder.Decode(&pong)
-			fmt.Printf("Got pong %s, matches? %b\n", pong.Token, sentPing.Token == pong.Token)
+			fmt.Printf("Got pong, token is '%s', matches? %b\n", pong.Token, sentPing.Token == pong.Token)
 		default:
 			fmt.Printf("Unknown packet of type %d incoming\n", messageType)
 		}
@@ -141,7 +232,7 @@ func infoScreen(gui fyne.App) fyne.CanvasObject {
 
 		widget.NewHBox(layout.NewSpacer(),
 			widget.NewHyperlink("code", parseURL("https://github.com/coderobe/andromeda")),
-		layout.NewSpacer()),
+			layout.NewSpacer()),
 		layout.NewSpacer(),
 		widget.NewLabelWithStyle("Create a network in the `Host` tab...", fyne.TextAlignCenter, fyne.TextStyle{}),
 		widget.NewLabelWithStyle("...or join one with `Join`", fyne.TextAlignCenter, fyne.TextStyle{}),
@@ -160,16 +251,16 @@ func infoScreen(gui fyne.App) fyne.CanvasObject {
 	)
 }
 
-func hostScreen(gui fyne.App) fyne.CanvasObject {
+func hostScreen() fyne.CanvasObject {
 	server := widget.NewEntry()
 	server.SetPlaceHolder("localhost:1234")
 	server.SetText("localhost:1234")
 
 	form := &widget.Form{
 		OnSubmit: func() {
+			setContainer(hostContainer, messageScreen("Host", "Starting server..."))
 			fmt.Println("Hosting as", server.Text)
 			go networkHost(server.Text)
-			setContainer(hostContainer, otherHostScreen())
 		},
 	}
 	form.Append("Listen address:port", server)
@@ -177,11 +268,11 @@ func hostScreen(gui fyne.App) fyne.CanvasObject {
 	return widget.NewGroup("Create network", form)
 }
 
-func otherHostScreen() fyne.CanvasObject {
-	return widget.NewGroup("Something", widget.NewLabel("hello world"))
+func messageScreen(title string, message string) fyne.CanvasObject {
+	return widget.NewGroup(title, widget.NewLabelWithStyle(message, fyne.TextAlignCenter, fyne.TextStyle{}))
 }
 
-func joinScreen(gui fyne.App) fyne.CanvasObject {
+func joinScreen() fyne.CanvasObject {
 	server := widget.NewEntry()
 	server.SetPlaceHolder("localhost:1234")
 	server.SetText("localhost:1234")
@@ -194,7 +285,7 @@ func joinScreen(gui fyne.App) fyne.CanvasObject {
 
 	form := &widget.Form{
 		OnSubmit: func() {
-			fmt.Println("Joining", server.Text)
+			setContainer(joinContainer, messageScreen("Join", "Connecting to network..."))
 			fmt.Println("Name:", username.Text)
 			fmt.Println("Pass:", password.Text)
 			var user User
@@ -211,6 +302,7 @@ func joinScreen(gui fyne.App) fyne.CanvasObject {
 }
 
 var hostContainer *widget.Box
+var joinContainer *widget.Box
 
 func setContainer(box *widget.Box, inner fyne.CanvasObject) {
 	box.Children = []fyne.CanvasObject{inner}
@@ -219,17 +311,19 @@ func setContainer(box *widget.Box, inner fyne.CanvasObject) {
 
 func main() {
 	hostContainer = widget.NewHBox()
+	joinContainer = widget.NewHBox()
 
 	gui := app.NewWithID("net.in.rob.andromeda")
 	win := gui.NewWindow("rob.in.net andromeda")
 	win.SetMaster()
 
-	setContainer(hostContainer, hostScreen(gui))
+	setContainer(hostContainer, hostScreen())
+	setContainer(joinContainer, joinScreen())
 
 	tabs := widget.NewTabContainer(
 		widget.NewTabItemWithIcon("Info", theme.InfoIcon(), infoScreen(gui)),
 		widget.NewTabItemWithIcon("Host", theme.HomeIcon(), hostContainer),
-		widget.NewTabItemWithIcon("Join", theme.NavigateNextIcon(), joinScreen(gui)))
+		widget.NewTabItemWithIcon("Join", theme.NavigateNextIcon(), joinContainer))
 	tabs.SetTabLocation(widget.TabLocationLeading)
 	win.SetContent(tabs)
 
