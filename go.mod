aa5e1ae410f6fa90899d935cec36851edfe42a79
Robin Broda <robin@broda.me>
2020-04-29 23:14:30 +0200

    Implement login/registration flow

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/coderobe/securenet v0.0.0-20200429180608-6443cef849ee
 	github.com/sethvargo/go-diceware v0.2.0
 	github.com/vmihailenco/msgpack/v4 v4.3.11
+	golang.org/x/crypto v0.0.0-20200423211502-4bdfaf469ed5
 )
--- main.go
+++ main.go
@@ -16,22 +16,42 @@ import (
 	"github.com/coderobe/securenet"
 	"github.com/sethvargo/go-diceware/diceware"
 	"github.com/vmihailenco/msgpack/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
-type User struct {
-	Name     string
-	Password string
+type Message struct {
+	ID     int
+	Packet interface{}
 }
 
+type User struct {
+	Name           string
+	Password       string
+	HashedPassword []byte
+	Connected      bool
+	SendChannel    chan Message
+}
+
+var Users []User
+
 const (
-	packetPing = iota
-	packetPong = iota
+	packetPing       = iota
+	packetPong       = iota
+	packetAuth       = iota
+	packetAuthStatus = iota
 )
 
 type MessagePing struct {
 	Token string
 }
 type MessagePong MessagePing
+type MessageAuth struct {
+	Username string
+	Password string
+}
+type MessageAuthStatus struct {
+	Success bool
+}
 
 func boundSendMessage(encoder *msgpack.Encoder, conn net.Conn) func(packetID int, message interface{}) (err error) {
 	return func(packetID int, message interface{}) (err error) {
@@ -55,7 +75,27 @@ func networkHost(server string) (err error) {
 		return
 	}
 
-	setContainer(hostContainer, messageScreen("Host", "Accepting connections"))
+	serverKeyWords := strings.Split(bytesToDiceware(pub[:]), " ")
+	serverKey := ""
+	for i, k := range serverKeyWords {
+		serverKey += k
+		if (i+1)%4 == 0 {
+			serverKey += "\n"
+		} else {
+			serverKey += " "
+		}
+	}
+
+	setContainer(hostContainer, widget.NewGroup("Accepting connections",
+		widget.NewVBox(
+			widget.NewLabelWithStyle("Your host is presenting this key:", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+			layout.NewSpacer(),
+			widget.NewLabelWithStyle(serverKey, fyne.TextAlignCenter, fyne.TextStyle{Monospace: true}),
+			layout.NewSpacer(),
+			widget.NewLabelWithStyle("Tell your users.", fyne.TextAlignCenter, fyne.TextStyle{Italic: true}),
+		),
+	))
+
 	for {
 		pConn, err := listener.Accept()
 		if err != nil {
@@ -78,13 +118,13 @@ func networkHost(server string) (err error) {
 				messageByte, err := conn.ReadByte()
 				if err != nil {
 					var netErr net.Error
-					if errors.As(err, netErr) {
+					if errors.As(err, &netErr) {
 						if netErr.Timeout() {
 							fmt.Println("Read timed out")
 							continue
 						}
 					}
-					panic("BBBBBBBBBBBBBBBBBBBBBb")
+					return
 				}
 				switch messageType := uint8(messageByte); messageType {
 				case packetPing:
@@ -97,6 +137,94 @@ func networkHost(server string) (err error) {
 					var pong MessagePong
 					decoder.Decode(&pong)
 					fmt.Printf("Got pong, token is '%s', matches? %b\n", pong.Token, sentPing.Token == pong.Token)
+				case packetAuth:
+					var auth MessageAuth
+					decoder.Decode(&auth)
+					var authStatus MessageAuthStatus
+					authStatus.Success = false
+
+					userExists := false
+					for _, user := range Users {
+						if user.Name == auth.Username {
+							// User exists
+							userExists = true
+							if bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(auth.Password)) == nil {
+								// Password correct
+								authStatus.Success = true
+								user.SendChannel = make(chan Message)
+								go func() {
+									for {
+										message := <-user.SendChannel
+										sendMessage(message.ID, message.Packet)
+									}
+								}()
+							}
+							break
+						}
+					}
+					if !userExists {
+						registerPermitChan := make(chan bool)
+
+						clientKeyWords := strings.Split(bytesToDiceware(conn.GetServerPublicKey()[:]), " ")
+						clientKey := ""
+						for i, k := range clientKeyWords {
+							clientKey += k
+							if (i+1)%4 == 0 {
+								clientKey += "\n"
+							} else {
+								clientKey += " "
+							}
+						}
+						oldContainer := getContainer(hostContainer)
+
+						setContainer(hostContainer, widget.NewGroup("Unknown user connection",
+							widget.NewVBox(
+								widget.NewLabelWithStyle("A previously unknown user has connected", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+								layout.NewSpacer(),
+								widget.NewLabelWithStyle("Username: '"+auth.Username+"'", fyne.TextAlignCenter, fyne.TextStyle{Monospace: true}),
+								layout.NewSpacer(),
+								widget.NewLabelWithStyle("The user is presenting this key:", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+								layout.NewSpacer(),
+								widget.NewLabelWithStyle(clientKey, fyne.TextAlignCenter, fyne.TextStyle{Monospace: true}),
+								layout.NewSpacer(),
+
+								widget.NewGroup("Allow registration?",
+									fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+										widget.NewButton("No", func() {
+											fmt.Println("Disallowed registration for", auth.Username)
+											registerPermitChan <- false
+										}),
+										widget.NewButton("Yes", func() {
+											fmt.Println("Allowed registration for", auth.Username)
+											registerPermitChan <- true
+										}),
+									),
+								),
+							),
+						))
+						if <-registerPermitChan {
+							var newUser User
+							hashedPw, err := bcrypt.GenerateFromPassword([]byte(auth.Password), 10)
+							if err != nil {
+								authStatus.Success = false
+							} else {
+								authStatus.Success = true
+								newUser.Name = auth.Username
+								newUser.HashedPassword = hashedPw
+								newUser.SendChannel = make(chan Message)
+								go func() {
+									for {
+										message := <-newUser.SendChannel
+										sendMessage(message.ID, message.Packet)
+									}
+								}()
+								Users = append(Users, newUser)
+							}
+						}
+						setContainer(hostContainer, oldContainer)
+					}
+
+					sendMessage(packetAuthStatus, authStatus)
 				default:
 					fmt.Printf("Unknown packet of type %d incoming\n", messageType)
 				}
@@ -181,11 +309,35 @@ func networkJoin(server string, user User) (err error) {
 	if <-abortChan {
 		return
 	}
-	setContainer(joinContainer, messageScreen("Join", "Connected"))
+
+	publicKeyWords := strings.Split(bytesToDiceware(conn.GetPublicKey()[:]), " ")
+	publicKey := ""
+	for i, k := range publicKeyWords {
+		publicKey += k
+		if (i+1)%4 == 0 {
+			publicKey += "\n"
+		} else {
+			publicKey += " "
+		}
+	}
+	setContainer(joinContainer, widget.NewGroup("Confirm network keys",
+		widget.NewVBox(
+			widget.NewLabelWithStyle("Your client is identifying as:", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+			layout.NewSpacer(),
+			widget.NewLabelWithStyle(publicKey, fyne.TextAlignCenter, fyne.TextStyle{Monospace: true}),
+			layout.NewSpacer(),
+			widget.NewLabelWithStyle("Please tell your host\nto verify your connection.", fyne.TextAlignCenter, fyne.TextStyle{Italic: true}),
+		),
+	))
 
 	var sentPing MessagePing // hold on to last ping we sent for pong
 	sendMessage := boundSendMessage(msgpack.NewEncoder(conn), conn)
 	decoder := msgpack.NewDecoder(conn)
+
+	var auth MessageAuth
+	auth.Username = user.Name
+	auth.Password = user.Password
+	sendMessage(packetAuth, auth)
 
 	for {
 		messageByte, err := conn.ReadByte()
@@ -210,6 +362,16 @@ func networkJoin(server string, user User) (err error) {
 			var pong MessagePong
 			decoder.Decode(&pong)
 			fmt.Printf("Got pong, token is '%s', matches? %b\n", pong.Token, sentPing.Token == pong.Token)
+		case packetAuthStatus:
+			var authStatus MessageAuthStatus
+			decoder.Decode(&authStatus)
+			if authStatus.Success {
+				println("Auth success")
+				setContainer(joinContainer, messageScreen("Join", "Authenticated"))
+			} else {
+				println("Auth fail")
+				setContainer(joinContainer, messageScreen("Join", "Authentication failure"))
+			}
 		default:
 			fmt.Printf("Unknown packet of type %d incoming\n", messageType)
 		}
@@ -307,6 +469,9 @@ var joinContainer *widget.Box
 func setContainer(box *widget.Box, inner fyne.CanvasObject) {
 	box.Children = []fyne.CanvasObject{inner}
 	box.Refresh()
+}
+func getContainer(box *widget.Box) fyne.CanvasObject {
+	return box.Children[0]
 }
 
 func main() {
